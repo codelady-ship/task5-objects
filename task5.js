685599e912bbbb64f245ef987b7939d9bc99f715
codelady-ship <jalilova.antiqa@gmail.com>
2024-12-11 19:00:53 +0400

    modified

--- task5.js
+++ task5.js
@@ -3,10 +3,11 @@ Konsola doğum tarixini çıxarın.
 Adınız, cinsiniz və doğum tarixiniz olan user obyekti yaradın (ad, gender, birthday).
 Konsola doğum tarixini çıxarın.*/
 const person ={
-  ad:Ayten 
-  gender:
+  ad:"Ayten",
+  cins: "woman",
+  dogum_tarixi:"23.12.2000"
 }
-
+document.write (`${person.ad}nin dogum tarixi ${person.dogum_tarixi}-dur <br/>`)
 
 
 /*Tapşırıq-02
@@ -15,6 +16,13 @@ Obyektə yaş (yaş) əlavə edin və doğum tarixi silin.
 Bu obyektə yaş (yaş) əlavə edin və doğum tarixini silin.
 Alınmış obyekti konsola çıxarın.*/
 
+const uzv={
+  yasi:25,
+  dogumTarixi:""
+}
+document.write(`uzvun yasi ${uzv.yasi}dir`)
+
+
 /*Tapşırıq-03
 Obyektə metodlar əlavə edin.let
 Obyekt verilib. Ona yeni metodlar əlavə edin:
@@ -22,8 +30,15 @@ Obyekt verilib. Ona yeni metodlar əlavə edin:
   getYearsBeforeRetirement - pensiyaya qədər
 qalan illərin məbləğini qaytaran metoddur.
 Bu metodları çağırın.*/
-
-
+const bank={
+  member:"Eli",
+  yas:25,
+  adGunu:"15.05.2001",
+  pulu:25000
+ }
+ setAge(bank.yas)
+ document.write(` pensiyaya qədər
+qalan illərin məbləğini ${ getYearsBeforeRetirement(bank.pulu)}dir`)
 /*Tapşırıq-04
 Kalkulyator obyekti yaradın.
 Əsas riyazi hesablamaları yerinə yetirən və nəticəni yaddaşda saxlaya bilən kalkulyator obyekti yaradın.
