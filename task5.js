e3a2eb0931f4d7b49313563c49165927d52c6842
codelady-ship <jalilova.antiqa@gmail.com>
2024-12-11 18:19:49 +0400

    javascript task

---
+++ task5.js
@@ -0,0 +1,33 @@
+/*Tapşırıq-01
+Konsola doğum tarixini çıxarın.
+Adınız, cinsiniz və doğum tarixiniz olan user obyekti yaradın (ad, gender, birthday).
+Konsola doğum tarixini çıxarın.*/
+
+
+
+
+/*Tapşırıq-02
+Obyektə yaş (yaş) əlavə edin və doğum tarixi silin.
+İstifadəçi olan bir obyekt verilib.
+Bu obyektə yaş (yaş) əlavə edin və doğum tarixini silin.
+Alınmış obyekti konsola çıxarın.*/
+
+/*Tapşırıq-03
+Obyektə metodlar əlavə edin.let
+Obyekt verilib. Ona yeni metodlar əlavə edin:
+  setAge - istifadəçi yaşını dəyişdirən metoddur.
+  getYearsBeforeRetirement - pensiyaya qədər
+qalan illərin məbləğini qaytaran metoddur.
+Bu metodları çağırın.*/
+
+
+/*Tapşırıq-04
+Kalkulyator obyekti yaradın.
+Əsas riyazi hesablamaları yerinə yetirən və nəticəni yaddaşda saxlaya bilən kalkulyator obyekti yaradın.
+  - Obyekt yaradın və toplama, çıxma, vurma, bölmə üçün metodlar əlavə edin. */ 
+
+/*Tapşırıq-05. propertyTaker() adında funksiya yaradın. Arqument kimi obyekt (obj) və string (proprtyName) alsin. Hemin obyektin verdiyim propertyni ekrana cixarsin. Mes:
+
+propertyTaker({ qitə: 'Asiya', ölkə: 'Yaponiya'}, 'qitə') // 'Asiya'
+propertyTaker({ ölkə: 'İsveç', qitə: 'Avropa'}, 'ölkə') // ' İsveç'
+propertyTaker({ad:'roma', yaş:12}, 'ad') // 'roma'*/
